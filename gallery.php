f52166dbd4a65a68d99ce22f45918d8e496b35de
Claudio Donoso Cornejo <claudiodonosoc@gmail.com>
2013-12-09 02:48:29 -0300

    Implementa Pages:::gallery

---
+++ gallery.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lg - Smile</title>
+  <link rel="stylesheet" href="/css/reset.css"/>
+  <link rel="stylesheet" href="/css/default.css"/>
+  <link rel="stylesheet" href="/css/font-awesome.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="/js/jquery.js"></script>
+  <script type="text/javascript" src="/js/jquery.easing.js"></script>
+  <script type="text/javascript" src="/js/css3finalize.js"></script>
+  <script type="text/javascript" src="/js/modernizr.js"></script>
+  <script type="text/javascript" src="/js/selectivizr.js"></script>
+  <script type="text/javascript" src="/js/nwmatcher.js"></script>
+  <script type="text/javascript" src="/js/default.js"></script>
+</head>
+<body>
+  <div id="pag">
+      <header>
+        <h1>LG Life's Good</h1>
+
+        <h2>Un techo para mi</h2>
+      </header>
+
+      <section class="pages gallery">
+          <div class="top">
+            <h2>See the smiles!</h2>
+
+            <a href="/" class="home"><span class="fa fa-home"></span> Home <span class="fa fa-angle-right"></span></a>
+          </div>
+
+          <div class="items">
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+
+            <div class="item">
+              <a href="/img/pages-gallery-img-default.jpg">
+                <img src="/img/pages-gallery-img-default.jpg" alt="">
+
+                <p>Lucia Perez</p>
+
+                <span class="fa fa-search"></span>
+              </a>
+            </div>
+          </div>
+      </section>
+
+      <footer>LG</footer>
+  </div>
+</body>
+</html>
