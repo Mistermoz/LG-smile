04622ee0ddba2f3560bd9ae94eb7fb7b9c938823
Claudio Donoso Cornejo <claudiodonosoc@gmail.com>
2013-12-09 22:52:34 -0300

    Implementa Pages:::Create

---
+++ create.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lg - Smile</title>
+  <link rel="stylesheet" href="/css/reset.css"/>
+  <link rel="stylesheet" href="/css/default.css"/>
+  <link rel="stylesheet" href="/css/font-awesome.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="/js/jquery.js"></script>
+  <script type="text/javascript" src="/js/jquery.easing.js"></script>
+  <script type="text/javascript" src="/js/css3finalize.js"></script>
+  <script type="text/javascript" src="/js/modernizr.js"></script>
+  <script type="text/javascript" src="/js/selectivizr.js"></script>
+  <script type="text/javascript" src="/js/nwmatcher.js"></script>
+  <script type="text/javascript" src="/js/default.js"></script>
+</head>
+<body>
+  <div id="pag">
+      <header>
+        <h1>LG Life's Good</h1>
+
+        <h2>Un techo para mi</h2>
+      </header>
+
+      <section class="pages create">
+        <h1>Creating and smile</h1>
+
+        <div class="thumb">Smile</div>
+
+        <div class="content">
+          <div class="description">
+            <h2><span>1</span> Create your smile</h2>
+
+            <p>
+              A nice smile with a complicit wink. A contagious smile makes others want to smaile. Take a pic with your best LG Smile and pread it to help children in need from your country.
+            </p>
+          </div>
+
+          <div class="choice">
+            <h3>Add Photo</h3>
+
+            <a href="/#/create/webcamera" class="webcamera">Web camera</a>
+
+            <a href="/#/create/pc" class="pc">Pc</a>
+          </div>
+        </div>
+
+        <div id="take-photo" class="lightbox">
+            <div class="content">
+              <a href="/#/take-photo/close" class="close">X</a>
+
+              <div class="how">
+                <h3>
+                  <span></span>
+
+                  <strong>Make you best</br>LG Smile</strong>
+                </h3>
+
+                <p>Wink and smile! Help children in need get a tech lab by LG and techno.</p>
+
+                <a href="/#/take-photo/camera" class="take"><span class="fa fa-camera"></span> Take Photo</a>
+              </div>
+
+              <div class="thumb"></div>
+            </div>
+        </div>
+      </section>
+
+      <footer>LG</footer>
+  </div>
+</body>
+</html>
