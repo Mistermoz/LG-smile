697e43959ae761d27c117227f3b33b0abaff13cb
Claudio Donoso Cornejo <claudiodonosoc@gmail.com>
2013-12-11 01:54:18 -0300

    Implementa HTML Pages:::preview

---
+++ preview.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lg - Smile - Cards</title>
+  <link rel="stylesheet" href="css/reset.css"/>
+  <link rel="stylesheet" href="css/default.css"/>
+  <link rel="stylesheet" href="css/font-awesome.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.js"></script>
+  <script type="text/javascript" src="js/css3finalize.js"></script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <script type="text/javascript" src="js/selectivizr.js"></script>
+  <script type="text/javascript" src="js/nwmatcher.js"></script>
+  <script type="text/javascript" src="js/default.js"></script>
+</head>
+<body>
+  <div id="pag">
+      <header>
+        <h1>LG Life's Good</h1>
+
+        <h2>Un techo para mi</h2>
+      </header>
+
+      <section class="pages preview">
+        <h1>Creating and smile</h1>
+
+        <div class="content">
+          <div class="elements">
+            <div class="element-content">
+              <img src="img/pages-create-h1.png" alt="" class="logo">
+
+              <img src="img/pages-preview-content-card-bg.png" alt="" class="card-bg">
+
+              <img src="img/pages-create-take-photo-thumb.png" alt="" Class="photo-smile">
+            </div>
+
+            <div class="element-footer">
+              <img src="img/header-h1.png" alt="" class="lg">
+
+              <p>Share love! We can make all kids smile!</p>
+
+              <img src="img/header-h2.png" alt="" class="techo">
+            </div>
+          </div>
+
+          <div class="buttons">
+            <a href="card.php" class="again">Change Card</a>
+
+            <a href="share.php" class="accept">Share</a>
+          </div>
+        </div>
+      </section>
+
+      <footer>LG</footer>
+  </div>
+</body>
+</html>
