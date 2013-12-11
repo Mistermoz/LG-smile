fb1a3e5a6d172a5e119ca91df88a436090ce8d15
Claudio Donoso Cornejo <claudiodonosoc@gmail.com>
2013-12-11 01:52:37 -0300

    Implementa HTML Pages:::card

---
+++ card.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lg - Smile - Cards</title>
+  <link rel="stylesheet" href="css/reset.css"/>
+  <link rel="stylesheet" href="css/default.css"/>
+  <link rel="stylesheet" href="css/font-awesome.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.js"></script>
+  <script type="text/javascript" src="js/css3finalize.js"></script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <script type="text/javascript" src="js/selectivizr.js"></script>
+  <script type="text/javascript" src="js/nwmatcher.js"></script>
+  <script type="text/javascript" src="js/default.js"></script>
+</head>
+<body>
+  <div id="pag">
+      <header>
+        <h1>LG Life's Good</h1>
+
+        <h2>Un techo para mi</h2>
+      </header>
+
+      <section class="pages card">
+        <h1>Creating and smile</h1>
+
+        <div class="thumb">Smile</div>
+
+        <div class="content">
+          <div class="description">
+            <h2><span>2</span> Choose a card</h2>
+
+            <p>
+              Add a message to your LG Smile and spread it!
+            </p>
+          </div>
+
+          <div class="show">
+            <div class="items">
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Christmas</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>New Year</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Thanks</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Birthday</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Love</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Friendship</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Smile</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+
+              <div class="item">
+                <img src="img/pages-card-show-item.png" alt="" width="56">
+
+                <p>Holiday</p>
+
+                <span class="fa fa-check"></span>
+              </div>
+            </div>
+             <div class="buttons">
+               <input type="hidden" id="card-choosen" value="">
+
+               <a href="success.php" class="again"><span>X</span> Go back</a>
+
+               <a href="preview.php" class="accept"><span class="fa fa-check"></span> Preview!</a>
+             </div>
+          </div>
+        </div>
+      </section>
+
+      <footer>LG</footer>
+  </div>
+</body>
+</html>
