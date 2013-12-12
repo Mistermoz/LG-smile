80c8c0e6ee6faade2c4bfcb428ca9f7f19139dfe
Claudio Donoso Cornejo <claudiodonosoc@gmail.com>
2013-12-12 00:28:03 -0300

    Implementa Pages:::share

---
+++ share.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lg - Smile -Share</title>
+  <link rel="stylesheet" href="css/reset.css"/>
+  <link rel="stylesheet" href="css/default.css"/>
+  <link rel="stylesheet" href="css/font-awesome.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.js"></script>
+  <script type="text/javascript" src="js/css3finalize.js"></script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <script type="text/javascript" src="js/selectivizr.js"></script>
+  <script type="text/javascript" src="js/nwmatcher.js"></script>
+  <script type="text/javascript" src="js/default.js"></script>
+</head>
+<body>
+  <div id="pag">
+      <header>
+        <h1>LG Life's Good</h1>
+
+        <h2>Un techo para mi</h2>
+      </header>
+
+      <section class="pages share">
+        <h1>Creating and smile</h1>
+
+        <div class="thumb">Smile</div>
+
+        <div class="content">
+          <div class="description">
+            <h2>Thank you! You spreaded</br>your LG Smile</h2>
+
+            <p>
+              Your LG Smile has joined the <strong>thousands of LG Smiles</strong> created by people throughout Latin America in order to bring a smile to children in need.
+            </p>
+          </div>
+
+          <div class="thumbnail">
+            <img src="img/pages-share-show-thumb.png" alt="">
+          </div>
+
+          <div class="socials">
+            <p>Share Your Experience</p>
+
+            <a href="" class="facebook"><span class="fa fa-facebook"></span></a>
+
+            <a href="" class="twitter"><span class="fa fa-twitter"></span></a>
+          </div>
+        </div>
+
+        <div id="take-photo" class="lightbox">
+            <div class="content">
+              <a href="/#/take-photo/close" class="close">X</a>
+
+              <div class="how">
+                <h3>
+                  <span></span>
+
+                  <strong>Make you best</br>LG Smile</strong>
+                </h3>
+
+                <p>Wink and smile! Help children in need get a tech lab by LG and techno.</p>
+
+                <a href="/#/take-photo/camera" class="take"><span class="fa fa-camera"></span> Take Photo</a>
+              </div>
+
+              <div class="thumb"></div>
+            </div>
+        </div>
+
+        <div id="pc-photo" class="lightbox">
+        </div>
+      </section>
+
+      <footer>LG</footer>
+  </div>
+</body>
+</html>
